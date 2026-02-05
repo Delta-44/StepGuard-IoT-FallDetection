3644e62c086219895b6bc0891980cd7645bce0ba
koi89 <javiergonzalez4848@gmail.com>
2026-02-05 08:46:28 +0100

    fix interface esp32

--- backend/src/models/esp32.ts
+++ backend/src/models/esp32.ts
@@ -1,37 +1,8 @@
 export interface ESP32 {
-    //complete revision of the ESP32 model, do not toutch too much until the DB is ready
-    id: string;
     macAddress: string;
     name: string;
-  //  location: string; // e.g., "Living Room"
+    impact_count: number;
+    timestamp: Date;
     status: 'online' | 'offline' | 'maintenance';
-
-    // firmwareVersion: string;
-
     isFallDetected: boolean;
-    config: ESP32Config;
 }
-
-export interface ESP32Config {
-    fallDetectionSensitivity: 'low' | 'medium' | 'high';
-    reportIntervalMs: number;
-    ledEnabled: boolean;
-}
-
-// Optional: Factory function / Mock data generator since no DB is set up yet
-export const createDefaultESP32 = (id: string, mac: string): ESP32 => ({
-    id,
-    macAddress: mac,
-    name: `ESP32-${id}`,
-    // location: 'Unassigned',
-    status: 'offline',
-    //no tocar por ahora
-    // firmwareVersion: '1.0.0',
-   
-    isFallDetected: false,
-    config: {
-        fallDetectionSensitivity: 'medium',
-        reportIntervalMs: 60000,
-        ledEnabled: true
-    }
-});
